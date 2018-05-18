d936e36cde1d98919c7fdd465a73756feb441684
Dominic Minischetti III <domminischetti@gmail.com>
2018-05-18 13:35:14 -0700

    Improve search. Add more preliminary animations.

--- portfolio/src/app/command-list/command-list.component.ts
+++ portfolio/src/app/command-list/command-list.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { COMMANDS } from '../mock-commands';
+import { trigger, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-command-list',
   templateUrl: './command-list.component.html',
-  styleUrls: ['./command-list.component.css']
+  styleUrls: ['./command-list.component.css'],
+  animations: [
+    trigger('animation', [
+      transition('* => *', [
+        query(':leave', [
+          stagger(100, [
+            animate('0.5s', style({ opacity: 0 }))
+          ])
+        ], {optional: true}),
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(100, [
+            animate('0.5s', style({ opacity: 1 }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class CommandListComponent implements OnInit {
 
